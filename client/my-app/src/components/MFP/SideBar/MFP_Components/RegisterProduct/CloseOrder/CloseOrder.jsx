6f6f5680baa1ce797e99e16121e90d075e540a63
Monil Gandhi <gandhi.monil007@gmail.com>
2023-06-20 01:27:02 +0530

    blurry

--- client/my-app/src/components/MFP/SideBar/MFP_Components/RegisterProduct/CloseOrder/CloseOrder.jsx
+++ client/my-app/src/components/MFP/SideBar/MFP_Components/RegisterProduct/CloseOrder/CloseOrder.jsx
@@ -3,13 +3,17 @@ import '../CloseOrder/CloseOrder.css';
 import SideBar from '../../../Sidebar';
 
 const CloseOrder = () => {
+    const [hoveredRow, setHoveredRow] = useState(null);
+    const [extended, setExtended] = useState(false);
+
     const [orders, setOrders] = useState([
-        { id: 1, name: 'Order 1', isExtended: false, isHovered: false },
-        { id: 2, name: 'Order 2', isExtended: false, isHovered: false },
-        { id: 3, name: 'Order 3', isExtended: false, isHovered: false },
+        { id: 1, name: 'Order 1', isExtended: false },
+        { id: 2, name: 'Order 2', isExtended: false },
+        { id: 3, name: 'Order 3', isExtended: false },
     ]);
 
     const toggleOrderExtended = (orderId) => {
+        setExtended(true);
         setOrders((prevOrders) =>
             prevOrders.map((order) =>
                 order.id === orderId ? { ...order, isExtended: !order.isExtended } : order
@@ -17,28 +21,29 @@ const CloseOrder = () => {
         );
     };
 
-    const toggleOrderHovered = (orderId, isHovered) => {
-        setOrders((prevOrders) =>
-            prevOrders.map((order) =>
-                order.id === orderId ? { ...order, isHovered: isHovered } : order
-            )
-        );
-    };
     const handleOrderClose = (orderId) => {
-        // Add code here to handle order close
-        console.log(`Closing order ${orderId}`);
+        setExtended(false);
         setOrders((prevOrders) =>
             prevOrders.map((order) =>
                 order.id === orderId ? { ...order, isExtended: !order.isExtended } : order
             )
         );
 
+        console.log(`Closing order ${orderId}`);
     };
 
     const handleOrderPending = (orderId) => {
-        // Add code here to handle order pending
         console.log(`Marking order ${orderId} as pending`);
     };
+
+    const handleRowHover = (orderId) => {
+        setHoveredRow(orderId);
+    };
+
+    const handleRowLeave = () => {
+        setHoveredRow(null);
+    };
+
 
     return (
         <div style={{ display: 'flex' }}>
@@ -48,26 +53,29 @@ const CloseOrder = () => {
                 {orders.map((order) => (
                     <div
                         key={order.id}
-                        className={`orderRow ${order.isExtended ? 'extended' : ''} ${order.isHovered ? 'hovered' : ''
+                        className={`orderRow ${order.isExtended ? 'extended' : ''} ${order.id === hoveredRow ? 'hovered' : ''
                             }`}
-                        onMouseEnter={() => toggleOrderHovered(order.id, true)}
-                        onMouseLeave={() => toggleOrderHovered(order.id, false)}
+                        onMouseEnter={() => handleRowHover(order.id)}
+                        onMouseLeave={handleRowLeave}
                     >
                         <div>{order.name}</div>
-                        {order.isExtended ? (
+                        <button className="expandButton" onClick={() => toggleOrderExtended(order.id)}>
+                            {extended ? 'View Less' : 'Expand'}
+                            <span className={`arrow ${order.isExtended ? 'rotate' : ''}`}>&#9660;</span>
+                        </button>
+                        {order.isExtended && (
+
                             <div className="buttonsWrapper">
-                                <button onClick={() => handleOrderClose(order.id)}>
-                                    Close Order
-                                </button>
-                                <button onClick={() => handleOrderPending(order.id)}>
-                                    Order Still Pending
-                                </button>
+                                <button onClick={() => handleOrderClose(order.id)}>Close Order</button>
+                                <button onClick={() => handleOrderPending(order.id)}>Order Pending</button>
                             </div>
-                        ) : (
-                            <button onClick={() => toggleOrderExtended(order.id)}>
-                                Expand
-                            </button>
                         )}
+                        {/* {!order.isExtended && (
+                            <button className="expandButton" onClick={() => toggleOrderExtended(order.id)}>
+                                {order.id === hoveredRow ? 'View Less' : 'Expand'}
+                                <span className={`arrow ${order.isExtended ? 'rotate' : ''}`}>&#9660;</span>
+                            </button>
+                        )} */}
                     </div>
                 ))}
             </div>
@@ -76,27 +84,3 @@ const CloseOrder = () => {
 };
 
 export default CloseOrder;
-
-
-
-
-
-// import React from 'react'
-// import '../CloseOrder/CloseOrder.css';
-// import SideBar from '../../../Sidebar';
-
-
-// const CloseOrder = () => {
-//     return (
-//         <div style={{ "display": "flex", }}>
-//             <SideBar />
-//             <div className="mainWrapperCO">
-//                 <h1>Radhe Govind</h1>
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default CloseOrder;
-
--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const { register_product_func, find_delivery_savers_func } = require('../Controllers/functions.js');
+const { register_product_func, find_delivery_savers_func, close_order_func } = require('../Controllers/functions.js');
 
 
 router.post('/DeliverySaverApi/rkRegister', register_product_func);
 router.post('/DeliverySaverApi/rkGetSavers', find_delivery_savers_func);
-
+router.post('/DeliverySaverApi/rkCloseOrder', close_order_func)
 
 
 
