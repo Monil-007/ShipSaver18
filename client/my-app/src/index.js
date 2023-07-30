bc9e6304fa9c910475dd1e3ab835a328539e48f9
Monil Gandhi <gandhi.monil007@gmail.com>
2023-07-30 22:45:23 +0530

    k

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import appstore from "./store";
 import { Provider } from "react-redux";
+import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
